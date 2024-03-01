c0f3aeb6052841e135456af98e27630d6a7bde9a
Jagjeet Singh <jk1428071@gmail.com>
2024-03-01 17:00:37 +0530

    Third  commit

--- mysite/views.py
+++ mysite/views.py
@@ -2,9 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from employees.models import Employees
 
-def home(request):
+def index(request):
     employee=Employees.objects.all()
     context={
         'employee':employee
     }
-    return render(request, 'home.html',context)
+    return render(request, 'index.html',context)
